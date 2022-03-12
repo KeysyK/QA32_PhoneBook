1da1f95ffa6de88710b849e4834c01db262a0320
KeysyK <keysy9696@gmail.com>
2022-03-12 08:38:27 +0200

    Log in test(methods click, type, testbase)

---
+++ src/test/java/tests/RegistrationTests.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class RegistrationTests extends TestBase{
+
+
+    @Test
+    public void positiveRegistration(){
+
+    }
+}
+
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,43 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    WebDriver wd;
+
+    @BeforeMethod
+    public void init(){
+        //browser+https
+        wd = new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/");
+    }
+    @AfterMethod
+    public void tearDown(){
+        wd.quit();
+    }
+
+    public void type(By locator, String text) {
+        if (text != null) {
+            WebElement element = wd.findElement(locator);
+            element.click();
+            element.clear();
+            element.sendKeys(text);
+        }
+    }
+    public void click(By locator){
+        wd.findElement(locator).click();
+    }
+    public boolean isElementpresent(By locator){
+        return wd.findElements(locator).size()>0;
+    }
+}
+
